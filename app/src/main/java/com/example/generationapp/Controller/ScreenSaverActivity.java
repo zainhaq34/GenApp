cf049ed5fc814c395d04ae11b3b7c8d5cbdf20c6
zain <zainhaq34@gmail.com>
2019-06-22 06:14:03 +0500

    Check Network bug fixed

--- app/src/main/java/com/example/generationapp/Controller/ScreenSaverActivity.java
+++ app/src/main/java/com/example/generationapp/Controller/ScreenSaverActivity.java
@@ -6,26 +6,20 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Handler;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.VideoView;
-
 import com.example.generationapp.R;
-import com.karan.churi.PermissionManager.PermissionManager;
+
 
 public class ScreenSaverActivity extends AppCompatActivity {
 
     private static final String TAG = "";
     VideoView videoView;
     private static final long ALERT_DIALOG_TIME_OUT = 3000;
-    String folderName = "/Generation Video";
-
-    PermissionManager permissionManager;
-
 
     @SuppressLint("ClickableViewAccessibility")
     @Override
@@ -33,84 +27,6 @@ public class ScreenSaverActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_saver);
 
-//        File file = new File(Environment.getExternalStorageDirectory() + folderName);
-//        if (!file.exists()) {
-//
-//            file.mkdirs();
-//            Log.e(TAG, "Folder Name " + file.getName());
-//        }
-//        String myFolder = Environment.getExternalStorageDirectory() + "/" + folderName;
-//        File file = new File(myFolder);
-//        if (!file.exists()) {
-//            file.mkdir();
-//            if (!file.mkdir()) {
-//                Toast.makeText(this, myFolder + " can't be created.", Toast.LENGTH_SHORT).show();
-//
-//            } else
-//                Toast.makeText(this, myFolder + " can be created.", Toast.LENGTH_SHORT).show();
-//        }
-//        permissionManager = new PermissionManager() {
-//            @Override
-//            public void ifCancelledAndCanRequest(Activity activity) {
-//                // Do Customized operation if permission is cancelled without checking "Don't ask again"
-//                // Use super.ifCancelledAndCanRequest(activity); or Don't override this method if not in use
-//            }
-//
-//            @Override
-//            public void ifCancelledAndCannotRequest(Activity activity) {
-//                // Do Customized operation if permission is cancelled with checking "Don't ask again"
-//                // Use super.ifCancelledAndCannotRequest(activity); or Don't override this method if not in use
-//            }
-//
-//            @Override
-//            public List<String> setPermission() {
-//                // If You Don't want to check permission automatically and check your own custom permission
-//                // Use super.setPermission(); or Don't override this method if not in use
-//                List<String> customPermission=new ArrayList<>();
-//                customPermission.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-//                customPermission.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-//                return customPermission;
-//            }
-//        };
-//
-//        if (permissionManager.checkAndRequestPermissions(this)){
-//
-//            File folder = getFilesDir();
-//            File f = new File(folder, folderName);
-//
-//            if (!f.exists()) {
-//                f.mkdir();
-//                Toast.makeText(this, f + " can't be created.", Toast.LENGTH_SHORT).show();
-//            } else {
-//                Toast.makeText(this, f + " can be created.", Toast.LENGTH_SHORT).show();
-//            }
-//
-//            VideoStreamer();
-//
-//            videoView.setOnTouchListener(new View.OnTouchListener() {
-//                @Override
-//                public boolean onTouch(View view, MotionEvent motionEvent) {
-//
-//                    startActivity(new Intent(ScreenSaverActivity.this, WebActivity.class));
-//                    finish();
-//                    return false;
-//                }
-//            });
-//        }
-
-//
-//        File folder = getFilesDir();
-//        File f = new File(folder, folderName);
-//
-//        if (!f.exists()) {
-//            f.mkdir();
-//            Toast.makeText(this, f + " can't be created.", Toast.LENGTH_SHORT).show();
-//        } else {
-//            Toast.makeText(this, f + " can be created.", Toast.LENGTH_SHORT).show();
-//        }
-
-
-//
         VideoStreamer();
 
         videoView.setOnTouchListener(new View.OnTouchListener() {
@@ -174,8 +90,4 @@ public class ScreenSaverActivity extends AppCompatActivity {
         }, ALERT_DIALOG_TIME_OUT);
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        permissionManager.checkResult(requestCode,permissions, grantResults);
-    }
 }
